787d29a71b12f90fad1cc14624517adb7e001d6e
Sunny Kumar <sunny9102003kumar@gmail.com>
2025-11-10 20:40:55 -0500

    Added footer section

---
+++ app/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { VscCode } from "react-icons/vsc";
+
+const Footer = () => {
+  return (
+    <div className='mt-20'>
+        <div className='text-center'>
+            <VscCode className='w-36 mx-auto mb-2'/>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@
 
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Work from "./components/Work";
@@ -14,6 +15,7 @@ export default function Home() {
     <About />
     <Work />
     <Contact />
+    <Footer />
     </>
   );
 }
